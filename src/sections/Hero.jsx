0d927a5c9b622ece5ed08f74eb06c477472e2e34
sandya0 <66195253+sandya0@users.noreply.github.com>
2025-08-30 14:11:40 +0700

    finishing hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,11 +1,32 @@
 import React, { useRef } from "react";
 import { AnimatedTextLines } from "../components/AnimatedTextLines";
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { Canvas } from "@react-three/fiber";
+import { Planet } from "../components/planet";
+import { Environment, Lightformer, Float } from "@react-three/drei";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const isMobile = useMediaQuery({ maxWidth: 853});
   const contextRef = useRef(null);
   const headerRef = useRef(null);
   const aboutText = "I design and develop user-friendly websites.";
 
+  useGSAP(() => {
+    const tl = gsap.timeline();
+    tl.from(contextRef.current, {
+      y: "50vh",
+      duration: 1,
+      ease: "circ.out",
+    });
+    tl.from(headerRef.current, {
+      opacity: 0,
+      y: 20,
+      duration: 1,
+      ease: "circ.out",
+    }, "<+0.2");
+  }, []);
   return (
     <section className="flex flex-col justify-end min-h-screen">
       <div ref={contextRef}>
@@ -42,6 +63,47 @@ const Hero = () => {
           </div>
         </div>
       </div>
+      <figure 
+      className="absolute inset-0 -z-50"
+      style={{ width: "100vw", height: "100vh", background: "url('/path/to/your/image.jpg') no-repeat center center/cover" }}>
+        <Canvas 
+        shadows 
+        camera={{position: [0, 0, -10], 
+        fov: 17.5, near: 1, far:  20 }}>
+        <ambientLight intensity={0.5} />
+        <Float speed={0.5}>
+          <Planet scale={isMobile ? 0.7 : 1} />
+        </Float>
+        <Environment resolution={256}>
+          <group rotation={[-Math.PI / 3, 4, 1  ]}>
+            <Lightformer
+              form={"circle"}
+              intensity={2}
+              position={[0, 5, -9]}
+              scale={10}
+              />
+            <Lightformer
+              form={"circle"}
+              intensity={2}
+              position={[0, 3, 1]}
+              scale={10}
+              />
+            <Lightformer
+              form={"circle"}
+              intensity={2}
+              position={[-5, -1, 1]}
+              scale={10}
+              />            
+            <Lightformer
+              form={"circle"}
+              intensity={2}
+              position={[10, 1, 0]}
+              scale={16}
+              />
+          </group>
+        </Environment>
+        </Canvas>
+      </figure>
     </section>
   );
 };
